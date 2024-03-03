9f5cdb27af0fb6da81920cfa8ee90330c12d1bf5
stevetaidev <stevetaidev@gmail.com>
2024-03-03 16:14:46 +0700

    Refactor App component to use useState hook for show and count

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [appState, setAppState] = useState({
+    show: true,
+    count: 1,
+  })
+
+  const handleShow = () => {
+    // setShow(prevShow => !prevShow)
+    setAppState(prev => {
+      return {
+        ...prev,
+        show: !prev.show,
+      }
+    })
+  }
+  const handleCount = () => {
+    setAppState(prev => {
+      return {
+        ...prev,
+        count: prev.count + 1,
+      }
+    })
+  }
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {
+          appState.show &&
+          <a href='https://react.dev' target='blank'>
+            <img src={reactLogo} alt='react logo' />
+          </a>
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='card'>
+        <button onClick={handleShow}>Show/Hide React Logo</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <button onClick={handleCount}>Count is {appState.count}</button>
+      </div>
     </>
   )
 }
