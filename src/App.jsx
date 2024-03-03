5c0f5169fbb4f4d102c02c6fc87fbb52263cdff9
stevetaidev <stevetaidev@gmail.com>
2024-03-03 18:13:14 +0700

    Add character fetching functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
-  const [appState, setAppState] = useState({
-    show: true,
-    count: 1,
-  })
+  const [characterId, setCharacterId] = useState(1)
 
-  const handleShow = () => {
-    // setShow(prevShow => !prevShow)
-    setAppState(prev => {
-      return {
-        ...prev,
-        show: !prev.show,
-      }
-    })
-  }
-  const handleCount = () => {
-    setAppState(prev => {
-      return {
-        ...prev,
-        count: prev.count + 1,
-      }
-    })
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/${characterId}`)
+      .then(response => response.json)
+      .then(character => console.log(character))
+
+    return () => console.log('cleanup')
+  }, [characterId])
+
+
+  const handleCharacterId = () => {
+    setCharacterId(prevId => prevId + 1)
   }
   return (
     <>
       <div>
-        {
-          appState.show &&
-          <a href='https://react.dev' target='blank'>
-            <img src={reactLogo} alt='react logo' />
-          </a>
-        }
-      </div>
-
-      <div className='card'>
-        <button onClick={handleShow}>Show/Hide React Logo</button>
-      </div>
-      <div>
-        <button onClick={handleCount}>Count is {appState.count}</button>
+        <button onClick={handleCharacterId}>
+          Fetch Character by ID of {characterId}
+        </button>
       </div>
     </>
   )
